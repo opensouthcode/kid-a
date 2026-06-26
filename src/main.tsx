63a85fb1586635827f24cd269b34069f7ce1232d
tenacitas-paypa <tenacitas.paypa@gmail.com>
2026-06-26 10:53:20 +0200

    feat: wrap app in AppErrorBoundary

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import { AppErrorBoundary } from './components/AppErrorBoundary';
 import { DataLayerProvider } from './contexts/DataLayerContext';
 import { LocalDataLayerProvider } from './contexts/LocalDataLayerContext';
 import { I18nProvider } from './i18n/I18nProvider';
@@ -12,14 +13,16 @@ initializeMagicLinkSession();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <I18nProvider>
-      <DataLayerProvider>
-        <LocalDataLayerProvider>
-          <BrowserRouter basename={import.meta.env.BASE_URL}>
-            <App />
-          </BrowserRouter>
-        </LocalDataLayerProvider>
-      </DataLayerProvider>
-    </I18nProvider>
+    <AppErrorBoundary>
+      <I18nProvider>
+        <DataLayerProvider>
+          <LocalDataLayerProvider>
+            <BrowserRouter basename={import.meta.env.BASE_URL}>
+              <App />
+            </BrowserRouter>
+          </LocalDataLayerProvider>
+        </DataLayerProvider>
+      </I18nProvider>
+    </AppErrorBoundary>
   </StrictMode>,
 );
